355e6e28ea6002068017d2f3436f5dfdfd4cdbcb
Devin Simpson <devin2806@gmail.com>
2021-06-02 13:37:15 -0400

    introvars and functions

---
+++ assets/js/game.js
@@ -0,0 +1,21 @@
+/* Module 3 - Creating a Robot Gladiators game using JS and HTML
+for Con Solo Game Convention 
+Rules: 1) Complete in less than 24 hours; 2) No CSS; 3) Do not alter HTML
+other than the title */
+
+var playerName = window.prompt("What is your robot's name? ");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  //fight()
