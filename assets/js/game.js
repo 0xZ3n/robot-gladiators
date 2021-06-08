0318964704516561614367ac6c1b542baf27b87e
Devin Simpson <devin2806@gmail.com>
2021-06-08 18:28:02 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,27 +3,28 @@ for Con Solo Game Convention
 Rules: 1) Complete in less than 24 hours; 2) No CSS; 3) Do not alter HTML
 other than the title */
 
-var playerName = window.prompt("What is your robot's name? ");
+// var playerName = 'Clank McKrank';
+var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-//You can also log mulitple values at once like this 
+// You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Bezos3.0";
+var enemyNames = ["Roborto", "Amy Android","Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-  //Alert the players that the round is starting
-  window.alert("Welcome to Robot Gladiators!")
+// fight function
+var fight = function(enemyName) {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
 
-  //Prompt to start or avoid the game
-  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-  console.log(promptFight)
+  // ask player if they'd like to fight or run
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
-    // if player choses to fight, then fight
+  // if player choses to fight, fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
@@ -38,7 +39,7 @@ var fight = function() {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
-    // remove player's health by subtracting the amount set in the enemyAttack variable
+    // remove players's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -54,7 +55,7 @@ var fight = function() {
   } else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-  
+
     // if yes (true), leave fight
     if (confirmSkip) {
       window.alert(playerName + " has decided to skip this fight. Goodbye!");
@@ -65,9 +66,13 @@ var fight = function() {
     else {
       fight();
     }
+    // if player did not chose 1 or 2 in prompt
+  } else {
+    window.alert("You need to pick a valid option. Try again!");
   }
-  console.log(playerMoney)
-
 };
 
-fight();
+// run fight function to start game
+ for (var i = 0; i < enemyNames.length; i++) {
+   fight(enemyNames[i]);
+ }
